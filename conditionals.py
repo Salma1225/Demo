393442a02d7b1e692fd433a174bbda484fbc028a
salma1225 <salma_omar-1997@hotmail.co.uk>
2024-03-22 19:21:56 +0000

    first commit

---
+++ conditionals.py
@@ -0,0 +1,17 @@
+my_name = "Yoda"
+if_name_jedi = True
+
+#If statement
+if my_name == "Sidious":
+    print("This is not a Jedi")
+
+#Elif statement
+elif my_name == "Yoda":
+    print("Jedi master")
+    if_name_jedi == True
+    print("Are you sure?")
+    
+
+
+else:
+    print ("This must be Master Luke")    
---
+++ datatypes.py
@@ -0,0 +1,26 @@
+#intergers
+num1 = 10
+num2 = 20
+
+print(num1+num2)
+
+#floats
+flt1 = 3.14
+flt2 = 9.81
+
+print(flt1+flt2)
+
+#strings
+my_name = 'Salma'
+my_fav_animal = "elephant"
+
+print(my_name, my_fav_animal)
+
+print("I like to visit Luke's house!")
+
+#Boolean
+is_age_18 = True
+if_raining = False 
+
+#Nontype
+default_value = none 
